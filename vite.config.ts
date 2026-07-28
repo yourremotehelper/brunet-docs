9d4eaa0041f8ed92500232c08f489e39e4c56315
yourremotehelper <your.remote.helper@gmail.com>
2026-07-28 19:36:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,11 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  nitro: {
+    cloudflare: {
+      wrangler: {
+        keep_vars: true,
+      },
+    },
+  },
 });
